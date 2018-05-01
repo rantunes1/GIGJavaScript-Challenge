5b4736812d7d09a4876f6ae0f751dd57d81f2e37
rantunes1 <rantunes1@gmail.com>
2018-05-01 17:52:12 +0200

    remove unused route

--- js/app.js
+++ js/app.js
@@ -2,20 +2,17 @@ angular.module('gig', ['ngRoute'])
   .config(function ($routeProvider, $locationProvider) {
     $locationProvider.hashPrefix('')
     $routeProvider
-      .when('/', {
-        templateUrl: 'contacts-list.html',
-        controller: 'contactListCtrl'
-      })
-      .when('/edit/:contact', {
-        templateUrl: 'contact-form.html',
-        controller: 'contactCtrl'
-      })
-      .when('/add', {
-        templateUrl: 'contact-form.html',
-        controller: 'contactCtrl'
-      })
-      .when('/delete/:contact', {
-        controller: 'contactCtrl'
-      })
-      .otherwise({redirectTo: '/'})
+        .when('/', {
+            templateUrl: 'contacts-list.html',
+            controller: 'contactListCtrl'
+        })
+        .when('/edit/:contact', {
+            templateUrl: 'contact-form.html',
+            controller: 'contactCtrl'
+        })
+        .when('/add', {
+            templateUrl: 'contact-form.html',
+            controller: 'contactCtrl'
+        })
+        .otherwise({redirectTo: '/'})
   });
